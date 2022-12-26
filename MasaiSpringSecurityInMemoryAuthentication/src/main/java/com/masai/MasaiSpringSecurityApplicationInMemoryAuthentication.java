319d4de0477b15bbc9691d96df7b27a4b2d38bd9
Shivam <shiarora4002@gmail.com>
2022-12-26 18:28:42 +0530

    InMemoryAuthentication

---
+++ MasaiSpringSecurityInMemoryAuthentication/src/main/java/com/masai/MasaiSpringSecurityApplicationInMemoryAuthentication.java
@@ -0,0 +1,13 @@
+package com.masai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MasaiSpringSecurityApplicationInMemoryAuthentication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MasaiSpringSecurityApplicationInMemoryAuthentication.class, args);
+	}
+
+}
---
+++ MasaiSpringSecurityInMemoryAuthentication/src/main/java/com/masai/configure/ProjectSecurityConfig.java
@@ -0,0 +1,46 @@
+package com.masai.configure;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class ProjectSecurityConfig {
+
+	
+	@Bean
+	public SecurityFilterChain masaiSecurityFilter(HttpSecurity http) throws Exception {
+		
+		http.authorizeHttpRequests( (auth)->auth
+				.requestMatchers("/masai/welcomeP").authenticated()
+				.requestMatchers("/masai/employee/admin").hasRole("admin")
+				.requestMatchers("/masai/welcome","/masai/employee/register").permitAll()
+		).csrf().disable()
+		.httpBasic();
+
+		return http.build();
+	}
+	
+	@Bean
+	public InMemoryUserDetailsManager userDetails() {
+		
+		InMemoryUserDetailsManager userDetailsService = new InMemoryUserDetailsManager();
+	  UserDetails admin = User.withUsername("shivam").password("1234").authorities("admin").build();
+	    UserDetails user = User.withUsername("user").password("12345").authorities("read").build();
+	    userDetailsService.createUser(admin);
+	    userDetailsService.createUser(user);
+	    return userDetailsService;
+	}
+	
+	 @Bean
+	 public PasswordEncoder passwordEncoder() {
+	        return NoOpPasswordEncoder.getInstance();
+	 }
+}
